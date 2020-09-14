ad7c2daafaf8eb844b96b7bd72bfd811221d6e0d
HankJiang <jianghan.ah@foxmail.com>
2020-09-14 17:38:44 +0800

    docker

--- src/request.js
+++ src/request.js
@@ -1,7 +1,9 @@
-const domain = process.env.PROJECT_ENV === 'production' ? 'http://http://81.68.211.167/:9001' : 'http://localhost:9001'
+const baseURL = process.env.NODE_ENV === 'production' ? 'http://81.68.211.167/:9001' : 'http://localhost:9001';
+
+console.log(baseURL);
 
 export const request = require('axios').create({
-    baseURL: domain,
+    baseURL: baseURL,
     timeout: 10000,
     headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'}
 });
